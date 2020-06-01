2e9dfca59c39d01ffce8d0d48d8007346c1cc508
Gabriel Akinyosoye <gabrielfemi799>
2020-06-01 18:57:39 -0400

    add http routing

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Hello world")
+	http.HandleFunc("'/", Index)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+
 }
