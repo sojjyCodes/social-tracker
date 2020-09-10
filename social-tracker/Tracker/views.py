6a8c9185e5ead1438770560318564887a25e8c35
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-09-10 11:39:52 +0100

    Updates

--- social-tracker/Tracker/views.py
+++ social-tracker/Tracker/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+from pprint import pprint
 
 # Create your views here.
 def home(request):
@@ -10,3 +12,17 @@ def search(request):
 
 def result(request):
     return render(request, 'social-tracker/results.html')
+
+def api():
+
+    # github username
+    username = "sojjyCodes"
+
+    # url to request
+    url = f"https://api.github.com/users/{username}"
+
+    # make the request and return the json
+    user_data = requests.get(url).json()
+    
+    # pretty print JSON data
+    pprint(user_data)
